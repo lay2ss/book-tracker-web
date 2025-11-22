74e20162f9d95c2cca772c01df952e9ea42ce542
lay2ss <lais222lai@gmail.com>
2025-11-22 16:23:46 -0300

    add login component

---
+++ src/components/Login.tsx
@@ -0,0 +1,11 @@
+import '../Login.css'
+
+const Login = () => {
+  return ( 
+    <section className='flex h-screen w-full justify-center'>
+        <main className='bg-white h-200 w-[90vw] md:w-[70vw] rounded-2xl my-auto'></main>
+    </section>
+  )
+}
+
+export default Login
