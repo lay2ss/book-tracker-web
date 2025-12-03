340ae152da035d54996a8363240c7af946e33762
lay2ss <lais222lai@gmail.com>
2025-12-03 16:53:11 -0300

    style(home page): update UI for mobile devices

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,17 @@
+import Book from "../components/Book";
 
 const HomePage = () => {
   return (
     <section className='flex h-screen w-full font-inter overflow-hidden flex-col p-5 relative mx-auto'>
-        <div className="flex justify-between">
+        <div className="flex justify-between w-[90vw] md:w-[70vw] lg:w-[60vw] mx-auto">
             <img src="src/assets/logo/logo_name.png" alt="logo name" className='w-40' />
             <div className="flex my-auto gap-3">
                 <img src="src/assets/icon/placeholder_profile.png" alt="profile picture" className="w-min h-min cursor-pointer" />
                 <img src="src/assets/icon/menu_bar.svg" alt="menu bar icon" className="h-min my-auto cursor-pointer" />
             </div>
         </div>
-        <main className='bg-white w-[90vw] md:w-[70vw] rounded-2xl gray-shadow mx-auto absolute -bottom-5 left-1/2 transform -translate-x-1/2 h-[92vh] gray-text'>
+        <main className='bg-white w-[90vw] md:w-[70vw] lg:w-[60vw] rounded-2xl gray-shadow mx-auto absolute -bottom-5 left-1/2 transform -translate-x-1/2 h-[92vh] gray-text'>
+        <div className="flex flex-col mx-auto">
             <div className="p-5">
                 <div className="relative w-full">
                     <input type="text" placeholder="Search for a book" className="border-none py-2 px-3 rounded-md outline-none focus:ring-1 focus:ring-[#FF6C00] bg-light-orange w-full"/>
@@ -22,7 +24,7 @@ const HomePage = () => {
                 </div>
             </div>
             <div className="px-5">
-                <div className="bg-light-orange flex py-4 px-5 justify-between rounded-md border border-[#FF6C00] flex-wrap text-sm">
+                <div className="bg-light-orange flex py-4 px-5 justify-between rounded-md border border-[#FF6C00] flex-wrap text-sm sm:text-[16px] max-w-[600px] mx-auto">
                     <div className="flex items-center">
                     <div>
                         <img src="src/assets/icon/book.svg" alt="book icon" />
@@ -37,8 +39,16 @@ const HomePage = () => {
                     </div>
                 </div>
             </div>
-            <div className="p-5 gray-text font-bold">
-                <h1 className="text-2xl">Currently reading</h1>
+        </div>
+            <div className="p-5 gray-text">
+                <h1 className="text-2xl font-bold">Currently reading</h1>
+                <div className="flex gap-3 sm:gap-4 pt-5 overflow-x-auto pb-5 font-inter font-semibold text-sm sm:text-[16px]">
+                    <Book 
+                        current={60}
+                        total={140}
+                        bookCover={"src/assets/icon/placeholder.png"}
+                        bookTitle={"[Book title]"}/>
+                </div>
             </div>
         </main>
     </section>
