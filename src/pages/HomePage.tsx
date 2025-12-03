9525893e8d2f990dd95c56062f06e289928e356b
lay2ss <lais222lai@gmail.com>
2025-12-02 23:02:22 -0300

    style(home page): add UI for mobile devices

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,48 @@
+
+const HomePage = () => {
+  return (
+    <section className='flex h-screen w-full font-inter overflow-hidden flex-col p-5 relative mx-auto'>
+        <div className="flex justify-between">
+            <img src="src/assets/logo/logo_name.png" alt="logo name" className='w-40' />
+            <div className="flex my-auto gap-3">
+                <img src="src/assets/icon/placeholder_profile.png" alt="profile picture" className="w-min h-min cursor-pointer" />
+                <img src="src/assets/icon/menu_bar.svg" alt="menu bar icon" className="h-min my-auto cursor-pointer" />
+            </div>
+        </div>
+        <main className='bg-white w-[90vw] md:w-[70vw] rounded-2xl gray-shadow mx-auto absolute -bottom-5 left-1/2 transform -translate-x-1/2 h-[92vh] gray-text'>
+            <div className="p-5">
+                <div className="relative w-full">
+                    <input type="text" placeholder="Search for a book" className="border-none py-2 px-3 rounded-md outline-none focus:ring-1 focus:ring-[#FF6C00] bg-light-orange w-full"/>
+                    <button className="right-12 top-2 icon-style">
+                        <img src="src/assets/icon/search.svg" alt="search icon"/>
+                    </button>
+                    <button className="right-3 top-2 icon-style">
+                        <img src="src/assets/icon/filter.svg" alt="filter icon"/>
+                    </button>
+                </div>
+            </div>
+            <div className="px-5">
+                <div className="bg-light-orange flex py-4 px-5 justify-between rounded-md border border-[#FF6C00] flex-wrap text-sm">
+                    <div className="flex items-center">
+                    <div>
+                        <img src="src/assets/icon/book.svg" alt="book icon" />
+                    </div>
+                        <p className="ml-1"><span className="font-bold">[3]</span> books this month</p>
+                    </div>
+                    <div className="flex items-center">
+                    <div>
+                        <img src="src/assets/icon/calendar.svg" alt="calendar icon" />
+                    </div>
+                        <p className="ml-1">Reading streak: <span className="font-bold">[5 days]</span></p>
+                    </div>
+                </div>
+            </div>
+            <div className="p-5 gray-text font-bold">
+                <h1 className="text-2xl">Currently reading</h1>
+            </div>
+        </main>
+    </section>
+  )
+}
+
+export default HomePage
