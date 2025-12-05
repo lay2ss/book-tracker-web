09af785e1b4b94b0bc52a75af67aa5e88df185f4
lay2ss <lais222lai@gmail.com>
2025-12-04 23:23:19 -0300

    feat: add props support to BookCard component

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,32 +1,40 @@
 import { useState } from "react";
+interface BookCardProps{
+    bookDescription: string;
+    bookCover: string;
+    bookTitle: string;
+    authorName: string;
+    year: number;
+    rate: number;
+}
 
-const BookCard = () => {
+const BookCard: React.FC<BookCardProps> = ({ bookDescription, bookCover, bookTitle, authorName, year, rate }) => {
     const [isFavorite, setIsFavorite] = useState(false);
     const toggleState = () => setIsFavorite(!isFavorite);
 
   return (
-    <div className='flex font-inter bg-light-orange rounded-2xl w-fit p-5 gray-text flex-col max-w-[450px]'>
+    <div className='flex font-inter bg-light-orange rounded-2xl w-fit p-5 gray-text flex-col max-w-[450px] shrink-0'>
         <div className="flex gap-5 overflow-auto">
             <div className="relative flex">
-                <img src="src/assets/icon/placeholder.png" alt=""/>
+                <img src={bookCover} alt="bookCover"/>
                 <div className="orange-bg absolute bottom-1 left-1 rounded-xl text-white w-fit py-1 px-2 text-sm tracking-wider">
-                    <p>[10/10]</p>
+                    <p>{rate}/10</p>
                 </div>
             </div>
-            <div className="flex flex-col justify-between w-[350px]">
+            <div className="flex flex-col justify-between max-w-[165px]">
                 <div>
-                    <h2 className="text-xl font-bold">[Book Title]</h2>
-                    <p>[Book Description Lorem ipsum dolor sit amet consectetur adipisicing elit.]</p>
+                    <h2 className="text-xl font-bold">{bookTitle}</h2>
+                    <p>{bookDescription}</p>
                 </div>
                 <div className="bg-white rounded-xl w-fit py-1 px-2 text-sm">
                     <p>
-                        [Author name]
+                        {authorName}
                     </p>
                 </div>
             </div>
         </div>
         <div className="flex justify-between mt-2">
-            <p className="text-sm">Read in [year]</p>
+            <p className="text-sm">Read in {year}</p>
             <button className="transition-transform active:scale-80" onClick={toggleState}>
                 <img src={isFavorite? "src/assets/icon/star_active.svg" : "src/assets/icon/star.svg"} alt="star-icon"/>
             </button>
