7ce64088091c299bbb54d039de0b0f3278c810a8
lay2ss <lais222lai@gmail.com>
2025-12-08 23:25:14 -0300

    update DesktopNav component

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -2,7 +2,13 @@ import { navData } from "../data/constants"
 
 const DesktopNav = () => {
   return (
-    <></>
+    <><ul className="flex gap-5 items-center gray-text">
+        {
+            navData.map((item) => 
+              (<li className="li-style">{item}</li>)
+            )
+        }
+      </ul></>
   )
 }
 
