9cc152065d8020bda3b112ee893050ca9e4cc091
lay2ss <lais222lai@gmail.com>
2025-12-08 23:24:53 -0300

    update MobileNav component

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,19 +1,28 @@
 import { navData } from "../data/constants"
 
-const MobileNav = () => {
+interface MobileNavProps {
+  toggle: any;
+}
+
+const MobileNav: React.FC<MobileNavProps> = ({toggle}) => {
   return (
-    <ul className="blur-effect px-5 py-14 gray-text flex items-center flex-col gap-3">
-        {
-          navData.map((item) => 
-            (<li className="li-style">{item}</li>)
-          )
-        }
-        <li>
-            <button className="button-style-outline border-[#FF6C00] orange-text">
-                Sign Out
-            </button>
-        </li>
-    </ul>
+      <>
+      <button onClick={toggle} className="cursor-pointer">
+        <img src="src/assets/icon/close.svg" alt="close icon" className="fixed z-10 top-9 right-5" />
+      </button>
+      <ul className="blur-effect px-5 py-14 gray-text flex items-center flex-col gap-3">
+          {
+            navData.map((item) => 
+              (<li className="li-style">{item}</li>)
+            )
+          }
+          <li>
+              <button className="button-style-outline border-[#FF6C00] orange-text">
+                  Sign Out
+              </button>
+          </li>
+      </ul>
+      </>
   )
 }
 
