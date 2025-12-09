d423e26685575afcc28f8f437d384f49d1b6ded8
lay2ss <lais222lai@gmail.com>
2025-12-08 23:26:42 -0300

    add DesktopNav component to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Book from "../components/Book";
 import BookCard from "../components/BookCard";
 import MobileNav from "../components/MobileNav";
+import DesktopNav from "../components/DesktopNav";
 
 const HomePage = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -9,21 +10,29 @@ const HomePage = () => {
 
   return (
     <section className='flex h-screen w-full font-inter flex-col p-5 relative mx-auto'>
-        <div className="flex justify-between w-[90vw] md:w-[70vw] lg:w-[60vw] mx-auto">
+        <div className="flex justify-between w-[90vw] lg:w-[80vw] max-w-[1200px] mx-auto md:bg-white md:rounded-xl md:px-4 px-2">
                 <img src="src/assets/logo/logo_name.png" alt="logo name" className='w-40 h-min' />
+            <div className="hidden md:flex">
+                <DesktopNav/>
+            </div>
             <div className="flex gap-3 my-auto">
                 <img src="src/assets/icon/placeholder_profile.png" alt="profile picture" className="w-min h-min cursor-pointer z-5"/>
-                <div className="relative flex  md:hidden">
+                <button className="hidden md:flex cursor-pointer active:scale-95">
+                    <img src="src/assets/icon/logout.svg" alt="log out icon" />
+                </button>
+                <div className="relative flex md:hidden">
                     <button onClick={toggleMenu} className="z-10">
-                        <img src={isOpen? 'src/assets/icon/close.svg' : 'src/assets/icon/menu_bar.svg'} alt="menu bar icon" className="h-min my-auto cursor-pointer"/>
+                        <img src={isOpen? 'src/assets/icon/close.svg' : 'src/assets/icon/menu_bar.svg'} alt="menu bar icon" className={`h-min my-auto cursor-pointer ${isOpen? 'hidden' : 'block'}`}/>
                     </button>
                     <div className={`fixed right-0 top-0 z-5 h-screen ${isOpen? 'flex' : 'hidden'}`}>
-                        <MobileNav/>
+                        <MobileNav
+                        toggle={toggleMenu}
+                        />
                     </div>
                 </div>
             </div>
         </div>
-        <main className='bg-white w-[90vw] md:w-[70vw] lg:w-[60vw] rounded-2xl mx-auto  -bottom-5 left-1/2 transform -translate-x-1/2 gray-text h-[92vh] absolute  gray-shadow'>
+        <main className='bg-white w-[90vw] lg:w-[80vw] max-w-[1200px] rounded-2xl mx-auto  -bottom-5 left-1/2 transform -translate-x-1/2 gray-text h-[92vh] absolute  gray-shadow'>
         <div className="flex flex-col mx-auto">
             <div className="p-5">
                 <div className="relative w-full">
