509338ee7e6ac3962d306b1e6b63faed1e2619db
lay2ss <lais222lai@gmail.com>
2025-12-09 22:27:38 -0300

    update DesktopNav component

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -2,10 +2,14 @@ import { navData } from "../data/constants"
 
 const DesktopNav = () => {
   return (
-    <><ul className="flex gap-5 items-center gray-text">
+    <><ul className="flex gap-4 items-center gray-text">
         {
             navData.map((item) => 
-              (<li className="li-style">{item}</li>)
+              (
+              <div  key={item.id} className="flex items-center gap-1 hover:bg-[#FFF5ED] hover:rounded-xl p-2">
+                <li className="li-style">{item.li}</li>
+              </div>
+            )
             )
         }
       </ul></>
