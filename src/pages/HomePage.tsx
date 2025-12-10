b515e0692beea8e1b2e10d7a36e2571b67b0d627
lay2ss <lais222lai@gmail.com>
2025-12-10 14:52:03 -0300

    refactor: remove Nav from Homepage and add to App.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,40 +1,11 @@
-import { useState } from "react";
 import Book from "../components/Book";
 import BookCard from "../components/BookCard";
-import MobileNav from "../components/MobileNav";
-import DesktopNav from "../components/DesktopNav";
 
 const HomePage = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <section className='flex h-screen w-full font-inter flex-col p-5 relative mx-auto'>
-        <div className="flex justify-between w-[90vw] lg:w-[80vw] max-w-[1200px] mx-auto md:bg-white md:rounded-xl md:px-4 px-2">
-            <a href="/" className="cursor-pointer">
-                <img src="src/assets/logo/logo_name.png" alt="logo name" className='w-40 h-min' />
-            </a>
-            <div className="hidden md:flex">
-                <DesktopNav/>
-            </div>
-            <div className="flex gap-3 my-auto">
-                <img src="src/assets/icon/placeholder_profile.png" alt="profile picture" className="w-min h-min cursor-pointer z-5"/>
-                <button className="hidden md:flex cursor-pointer active:scale-95">
-                    <img src="src/assets/icon/logout.svg" alt="log out icon" />
-                </button>
-                <div className="relative flex md:hidden">
-                    <button onClick={toggleMenu} className="z-10">
-                        <img src={isOpen? 'src/assets/icon/close.svg' : 'src/assets/icon/menu_bar.svg'} alt="menu bar icon" className={`h-min my-auto cursor-pointer ${isOpen? 'hidden' : 'block'}`}/>
-                    </button>
-                    <div className={`fixed right-0 top-0 z-5 h-screen ${isOpen? 'flex' : 'hidden'}`}>
-                        <MobileNav
-                        toggle={toggleMenu}
-                        />
-                    </div>
-                </div>
-            </div>
-        </div>
-        <main className='bg-white w-[90vw] lg:w-[80vw] max-w-[1200px] rounded-2xl mx-auto  -bottom-23 left-1/2 transform -translate-x-1/2 gray-text h-screen absolute'>
+    <section className='h-screen w-full font-inter p-5 relative mx-auto'>
+        <main className='bg-white w-[90vw] lg:w-[80vw] max-w-[1200px] rounded-2xl mx-auto bottom-0 left-1/2 transform -translate-x-1/2 gray-text h-screen absolute'>
         <div className="flex flex-col mx-auto">
             <div className="p-5">
                 <div className="relative w-full">
