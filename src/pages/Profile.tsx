63371f89ed3bc01e4febca1ef795a5c898965943
lay2ss <lais222lai@gmail.com>
2025-12-13 15:18:30 -0300

    create Profile component

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+  return (
+    <section className='h-screen w-full font-inter p-5 relative mx-auto text-white'>
+        <main className='w-[90vw] lg:w-[80vw] max-w-[1200px] rounded-tl-2xl rounded-tr-2xl mx-auto bottom-0 left-1/2 transform -translate-x-1/2  h-screen absolute border-3 border-[#252033] p-5'>
+        
+        </main>
+    </section>
+  )
+}
+
+export default Profile
