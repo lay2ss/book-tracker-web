1edb47cd8350f87a68d87f39a218cde2474fdb2c
lay2ss <lais222lai@gmail.com>
2025-12-14 15:14:17 -0300

    create StatWidget component

---
+++ src/components/StatWidget.tsx
@@ -0,0 +1,22 @@
+interface StatWidgetProps{
+    stat: number;
+    icon: string;
+    text: string;
+    alt: string;
+}
+
+const StatWidget: React.FC<StatWidgetProps> = ({stat, icon, text, alt}) => {
+  return (
+            <div className="stats-style bg-dark-purple">
+                <div className="flex gap-2">
+                    <img src={icon} alt={`${alt} icon`} />
+                    <p>
+                        {text}
+                    </p>
+                </div>
+                <p className="purple-text mt-2">{stat}</p>
+            </div>
+  )
+}
+
+export default StatWidget
