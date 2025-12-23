7b293ec6179bd802498ff67129d447c642dcd1d6
lay2ss <lais222lai@gmail.com>
2025-12-22 22:05:01 -0300

    add new prop to BookReview.tsx

--- src/components/BookReview.tsx
+++ src/components/BookReview.tsx
@@ -2,17 +2,21 @@ interface BookReviewProps{
     cover: string;
     title: string;
     review?: string;
+    rate: number;
 }
 
-const BookReview: React.FC<BookReviewProps> = ({cover, title, review}) => {
+const BookReview: React.FC<BookReviewProps> = ({cover, title, review, rate}) => {
   return (
     <div className="flex shrink-0 text-center cursor-pointer">
-                <div className="flex gap-3">
+                <div className="flex w-full justify-between">
+                    <div className="flex gap-3">
                     <img src={cover} alt="book cover" className="w-20 h-30"/>
-                    <div className="">
-                        <h2 className="text-start text-lg">{title}</h2>
-                        <p className=" text-start opacity-90">{review}</p>
+                      <div>
+                          <h2 className="text-start text-lg">{title}</h2>
+                          <p className=" text-start opacity-90">{review}</p>
+                      </div>
                     </div>
+                    <p className="purple-text justify-self-end">{rate}/10</p>
                 </div>
         </div>
   )
