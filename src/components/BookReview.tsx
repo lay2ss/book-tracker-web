dd408c821dd1dbc23b72de4b01ab47b284e620d1
lay2ss <lais222lai@gmail.com>
2025-12-23 16:01:37 -0300

    feat: integrate relative-time element into BookReview component

--- src/components/BookReview.tsx
+++ src/components/BookReview.tsx
@@ -1,19 +1,26 @@
+import "@github/relative-time-element";
 interface BookReviewProps{
     cover: string;
     title: string;
     review?: string;
     rate: number;
+    datetime: string;
 }
 
-const BookReview: React.FC<BookReviewProps> = ({cover, title, review, rate}) => {
+const BookReview: React.FC<BookReviewProps> = ({cover, title, review, rate, datetime}) => {
   return (
     <div className="flex shrink-0 text-center cursor-pointer">
                 <div className="flex w-full justify-between">
                     <div className="flex gap-3">
                     <img src={cover} alt="book cover" className="w-20 h-30"/>
-                      <div>
-                          <h2 className="text-start text-lg">{title}</h2>
-                          <p className=" text-start opacity-90">{review}</p>
+                      <div className="text-start justify-between flex flex-col">
+                        <div>
+                          <h2 className="text-lg">{title}</h2>
+                          <p className="opacity-90">{review}</p>
+                        </div>
+                          <div className="opacity-60 text-sm">
+                            <relative-time datetime={datetime}></relative-time>
+                          </div>
                       </div>
                     </div>
                     <p className="purple-text justify-self-end">{rate}/10</p>
