bb243c9efa7333ba0ee38db1325ddf3f95c61c42
lay2ss <lais222lai@gmail.com>
2025-12-25 15:00:53 -0300

    feat(profile): add edit profile picture button

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -7,7 +7,12 @@ const Profile = () => {
     <section className='h-screen w-full font-inter p-5 relative mx-auto text-white'>
         <main className='w-[90vw] lg:w-[80vw] max-w-[1200px] rounded-tl-2xl rounded-tr-2xl mx-auto bottom-0 left-1/2 transform -translate-x-1/2  h-screen absolute border-l-3 border-r-3 border-t-3 border-[#252033] p-5'>
         <div className="flex flex-wrap md:flex-nowrap w-full justify-center gap-5 items-center">
-          <img src="src/assets/icon/placeholder_profile2.png" alt="profile picture" className="w-min h-min cursor-pointer"/>
+          <div className="relative">
+            <img src="src/assets/icon/placeholder_profile2.png" alt="profile picture" className="w-min h-min"/>
+            <button className="bg-gray-100 w-6 h-6 rounded-full absolute top-10 left-9 cursor pointer p-0.5 cursor-pointer">
+              <img src="src/assets/icon/edit.svg" alt="edit icon" className=""/>
+            </button>
+          </div>
           <div className="flex md:gap-2  flex-wrap gap-2 md:justify-center md:flex-nowrap lg:gap-4">
             <StatWidget 
             stat={34}
