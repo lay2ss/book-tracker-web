78b28e63b457092f801aaf8af9b9f76fc7056883
lay2ss <lais222lai@gmail.com>
2025-12-28 16:16:01 -0300

    feat: update App.tsx to configure routes with conditional Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+
 import Nav from './components/Nav';
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 import Profile from './pages/Profile';
 
-function App() {
+function AppLayout() {
+  const location = useLocation();
+  const showNav = ["/", "/home", "/profile"].includes(location.pathname);
 
   return (
     <>
-    <Nav/>
-    <Profile/>
+      {showNav && <Nav />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <Router>
+      <AppLayout />
+    </Router>
+  );
+}
+
