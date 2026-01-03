4b962c9187e7e408ac299cd6d74f962b1965dcbb
lay2ss <lais222lai@gmail.com>
2026-01-03 16:02:11 -0300

    add Genres data

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -12,3 +12,21 @@ export const navData = [
         li: "Favorites",
     },
 ];
+
+export const Genresdata = [
+  { id: 1, genre: "Romance" },
+  { id: 2, genre: "Science Fiction" },
+  { id: 3, genre: "Fantasy" },
+  { id: 4, genre: "Mystery" },
+  { id: 5, genre: "Thriller" },
+  { id: 6, genre: "Horror" },
+  { id: 7, genre: "Biography" },
+  { id: 8, genre: "History" },
+  { id: 9, genre: "Poetry" },
+  { id: 10, genre: "Drama" },
+  { id: 11, genre: "Adventure" },
+  { id: 12, genre: "Children's Literature" },
+  { id: 13, genre: "Young Adult (YA)" },
+  { id: 14, genre: "Self-help" },
+  { id: 15, genre: "Educational/Academic" }
+]
