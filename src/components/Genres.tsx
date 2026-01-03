5a99f1282fdaa4d825bad84c5594819688ca56fe
lay2ss <lais222lai@gmail.com>
2026-01-03 16:04:17 -0300

    feat: implement genres component for easier book genre selection in settings

---
+++ src/components/Genres.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+interface GenresProps{
+    genre: string;
+}
+
+const Genres: React.FC<GenresProps> = ({genre}) => {
+
+    const [isChecked, setIsChecked] = useState(false);
+    const toggleState = () => setIsChecked(!isChecked);
+
+  return (
+    <button onClick={toggleState} className={`px-2 py-1 border-gray-700 border rounded-full cursor-pointer text-sm transition-transform active:scale-98 ${isChecked? "text-[#252033] bg-[#b99ef6] border-none" : ""}`}>{genre}</button>
+  )
+}
+
+export default Genres
