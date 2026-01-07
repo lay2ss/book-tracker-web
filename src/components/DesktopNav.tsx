f541495f52a05c35a75c8c6d0b5666e436eb7447
lay2ss <lais222lai@gmail.com>
2026-01-06 23:11:22 -0300

    fix typo

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -6,8 +6,8 @@ const DesktopNav = () => {
         {
             navData.map((item) => 
               (
-              <a href={`/${item.li.toLowerCase()}`}>
-                <div  key={item.id} className="flex items-center gap-1 hover:border hover:border-[#b99ef6] hover:rounded-xl p-2">
+              <a key={item.id} href={`/${item.li.toLowerCase()}`}>
+                <div className="flex items-center gap-1 hover:border hover:border-[#b99ef6] hover:rounded-xl p-2">
                     {location.pathname === `/${item.li.toLowerCase()}` || (location.pathname === "/" && item.li == "Home")  ? <li className="li-style purple-text">{item.li}</li> : <li className="li-style">{item.li}</li>}
                 </div>
               </a>
