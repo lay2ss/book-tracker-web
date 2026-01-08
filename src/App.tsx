e15901232dd3496d9817ca62ab99c57947f7cd7f
lay2ss <lais222lai@gmail.com>
2026-01-07 22:46:27 -0300

    feat(router): add new route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Profile from './pages/Profile';
 import Settings from "./pages/Settings";
+import BookDetails from "./components/BookDeatils";
 
 function AppLayout() {
   const location = useLocation();
-  const showNav = ["/", "/home", "/profile", "/settings"].includes(location.pathname);
+  const showNav = ["/", "/home", "/profile", "/settings"].includes(location.pathname) ||
+  location.pathname.startsWith("/book/");
 
   return (
     <>
@@ -18,6 +20,7 @@ function AppLayout() {
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/settings" element={<Settings />} />
+        <Route path="/book/:id" element={<BookDetails />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
