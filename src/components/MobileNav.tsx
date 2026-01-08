876f4846cc25c10d9728899d0602475a13ce4777
lay2ss <lais222lai@gmail.com>
2026-01-07 22:47:10 -0300

    refactor(nav): update icon usage to import instead of static src

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,4 +1,5 @@
-import { navData } from "../data/constants"
+import { navData } from "../data/constants";
+import closeIcon from "../assets/icon/close.svg";
 
 interface MobileNavProps {
   toggle: any;
@@ -8,7 +9,7 @@ const MobileNav: React.FC<MobileNavProps> = ({toggle}) => {
   return (
       <>
       <button onClick={toggle} className="cursor-pointer">
-        <img src="src/assets/icon/close.svg" alt="close icon" className="fixed z-10 top-9 right-5" />
+        <img src={closeIcon} alt="close icon" className="fixed z-10 top-9 right-5" />
       </button>
       <ul className="blur-effect px-5 py-14 flex items-center flex-col gap-3 text-white">
           {
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,11 @@
 import DesktopNav from "./DesktopNav";
 import MobileNav from "./MobileNav";
 import { useState } from "react";
+import logoutIcon from "../assets/icon/logout.svg";
+import profilePicture from "../assets/icon/placeholder_profile.png";
+import menuBarIcon from "../assets/icon/menu_bar.svg";
+import closeIcon from "../assets/icon/close.svg";
+import logoname from "../assets/logo/logo_name.svg";
 
 const Nav = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -9,21 +14,21 @@ const Nav = () => {
     <nav className='w-full font-inter p-5 relative mx-auto'>
         <div className="flex justify-between w-[90vw] lg:w-[80vw] max-w-[1200px] mx-auto md:bg-[#252033] md:rounded-xl md:px-4 px-2 md:py-2">
             <a href="/" className="cursor-pointer">
-                <img src="src/assets/logo/logo_name.svg" alt="logo name" className='w-40 h-min' />
+                <img src={logoname} alt="logo name" className='w-40 h-min' />
             </a>
             <div className="hidden md:flex">
                 <DesktopNav/>
             </div>
             <div className="flex gap-3 my-auto">
                 <a href="/profile" className={`${location.pathname === "/profile" ? "border-2 border-white rounded-full" : "border-none"}`}>
-                    <img src="src/assets/icon/placeholder_profile.png" alt="profile picture" className="w-min h-min cursor-pointer"/>
+                    <img src={profilePicture} alt="profile picture" className="w-min h-min cursor-pointer"/>
                 </a>
                 <button className="hidden md:flex cursor-pointer active:scale-95">
-                    <img src="src/assets/icon/logout.svg" alt="log out icon" />
+                    <img src={logoutIcon} alt="log out icon" />
                 </button>
                 <div className="relative flex md:hidden">
                     <button onClick={toggleMenu} className="z-10">
-                        <img src={isOpen? 'src/assets/icon/close.svg' : 'src/assets/icon/menu_bar.svg'} alt="menu bar icon" className={`h-min my-auto cursor-pointer ${isOpen? 'hidden' : 'block'}`}/>
+                        <img src={isOpen? closeIcon : menuBarIcon } alt="menu bar icon" className={`h-min my-auto cursor-pointer ${isOpen? 'hidden' : 'block'}`}/>
                     </button>
                     <div className={`fixed right-0 top-0 z-5 h-screen ${isOpen? 'flex' : 'hidden'}`}>
                         <MobileNav
