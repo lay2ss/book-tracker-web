a314ffaf48fec49c5c8f8540975b8496c1837511
lay2ss <lais222lai@gmail.com>
2026-01-15 23:20:00 -0300

    feat: integrate DatePicker into AddBook component

--- src/components/AddBook.tsx
+++ src/components/AddBook.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getBookById } from "../services/bookService";
+import DatePicker from "./DatePicker";
 
 const AddBook = () => {
     const { id } = useParams<{ id: string }>();
     const [book, setBook] = useState<any>(null);
+
+    const [checked, setChecked] = useState(false);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setChecked(e.target.checked);
+ 
 
     const [isActive, setIsActive] = useState(true);
     const [isActiveRead, setIsActiveRead] = useState(false);
@@ -36,11 +41,11 @@ const AddBook = () => {
     return (
     <section className='h-screen w-full font-inter p-5 relative mx-auto text-white'>
         <main className='w-[90vw] lg:w-[80vw] max-w-[1200px] rounded-tl-2xl rounded-tr-2xl mx-auto bottom-0 left-1/2 transform -translate-x-1/2 h-screen absolute border-l-3 border-r-3 border-t-3 border-[#252033] p-5'>
-        <h1 className="text-2xl font-bold purple-text text-center">Add Book to Library</h1>
-        <div className="flex justify-between gap-10 mt-5">
-            <div className="flex flex-col w-fit">
+        <h1 className="text-2xl font-bold purple-text text-center lg:text-start">Add Book to Library</h1>
+        <div className="flex justify-center gap-10 mt-5 flex-wrap lg:flex-nowrap lg:justify-between">
+            <div className="flex flex-col w-fit items-center lg:max-w-60  max-w-120">
                 <img src={book.coverImage || "src/assets/icon/placeholder.png"} alt={book.title} className="h-fit rounded-md gray-shadow min-w-50 w-fit" />
-                <h1 className="text-xl font-bold mt-2">{book.title}</h1>
+                <h1 className="text-xl font-bold mt-2 text-center lg:text-start">{book.title}</h1>
                 <p className="text-sm purple-text font-light">
                     {book.authors?.join(", ")}
                 </p> 
@@ -51,17 +56,27 @@ const AddBook = () => {
                         Reading Status
                     </h2>
                     <div className="flex mt-2 relative w-full">
-                        <button onClick={toggleState} className={isActive? "addButtonActived" : "addButton"}>Currently reading</button>
-                        <button onClick={toggleState}  className={`right-10 ${isActiveRead? "addButtonActived" : "addButton"}` }>Finished</button>
+                        <button onClick={toggleState} className={isActive? "addButtonActived" : "addButton"}>Reading</button>
+                        <button onClick={toggleState}  className={`right-5 md:right-10 ${isActiveRead? "addButtonActived" : "addButton"}` }>Finished</button>
                     </div>
                 </div>
                 <div>
                     <h2 className="text-xl font-bold mt-13">
                         Completion Date
                     </h2>
-                    <div className="flex gap-2 items-center">
-                        <input type="checkbox" name="" id="date" className="cursor-pointer appearance-none border-[#b99ef6] border w-4 h-4 rounded-md checked:bg-[#b99ef6]"/>
-                        <label htmlFor="date">Use today's date &#40;{monthYear}&#41;</label>
+                    <div className="flex flex-col lg:flex-row lg:gap-10 mt-2">
+                        <div className="w-fit relative">
+                            <div className={`bg-[#5453556c] p-5 w-30 ${checked? "absolute" : "hidden"}`}></div>
+                            <div className={`bg-[#5453556c] p-5 w-20 right-0 ${checked? "absolute" : "hidden"}`}></div>
+                            <DatePicker />  
+                        </div>
+                        <div className="flex gap-2 items-center mt-2 lg:mt-0">
+                            <input type="checkbox" name="" id="todaysDate" className="cursor-pointer appearance-none border-[#b99ef6] border w-4 h-4 rounded-md checked:bg-[#b99ef6]"
+                            checked={checked}
+                            onChange={handleChange}
+                            />
+                            <label htmlFor="todaysDate">Use today's date &#40;{monthYear}&#41;</label>
+                        </div>
                     </div>
                 </div>
             </div>
