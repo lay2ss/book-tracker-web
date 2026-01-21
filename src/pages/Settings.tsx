2998679afde792c3ff80727f038ff410ec82c006
lay2ss <lais222lai@gmail.com>
2026-01-20 22:30:38 -0300

    update(Settings.tsx): refreshed design implementation

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -6,9 +6,9 @@ const Settings = () => {
 
   return (
     <section  className='h-screen w-full font-inter p-5 relative mx-auto text-white'>
-        <main className='w-[90vw] lg:w-[80vw] max-w-[1200px] rounded-tl-2xl rounded-tr-2xl mx-auto bottom-0 left-1/2 transform -translate-x-1/2  h-screen absolute border-l-3 border-r-3 border-t-3 border-[#252033] p-5 flex gap-3 flex-col'>
-            <div className="bg-dark-purple p-4 rounded-xl">
-                <h1 className="text-xl font-bold purple-text text-start">Reading Goal</h1>
+        <main className='w-[90vw] lg:w-[80vw] max-w-[1200px] rounded-2xl mx-auto md:mt-2 py-20 flex gap-3 flex-col'>
+            <div className="border border-white/20 p-4 rounded-xl">
+                <h1 className="text-xl font-bold text-start">Reading Goal</h1>
                 <div className='flex flex-col'>
                   <div className='flex items-center'>
                     <p>Annual</p> 
@@ -19,13 +19,13 @@ const Settings = () => {
                     <p>Monthly</p>
                   </div>
                   <div className='flex items-center justify-between gap-2 flex-wrap md:flex-nowrap'>
-                    <input type="text" placeholder="Type a number" className='border border-gray-700 p-2 w-full md:w-1/3 rounded-md outline-none focus:ring-1 focus:ring-[#b99ef6] text-sm' />
+                    <input type="number" max={1000} min={0} className='border border-white/20 p-2 w-20 rounded-md outline-none focus:ring-1 focus:ring-[#b99ef6] text-sm' />
                     <button className='rounded-xl text-[#252033] text-xs font-bold tracking-wider transition-transform active:scale-95 bg-[#b99ef6] h-min py-3 px-5 mt-2 md:mt-0 cursor-pointer'>Save</button>
                   </div>
                 </div>
             </div>
-            <div className="bg-dark-purple p-4 rounded-xl">
-              <h1 className="text-xl font-bold purple-text text-start">Favorites Genres</h1>
+            <div className="border border-white/20 p-4 rounded-xl">
+              <h1 className="text-xl font-bold text-start">Favorites Genres</h1>
               <h2 className='mt-1'>Select your favorites genres to get better recommendations</h2>
               <div className='flex flex-wrap gap-2 mt-3'>
                 {Genresdata.map((item) => (
@@ -37,8 +37,8 @@ const Settings = () => {
                 ))}
               </div>
             </div>
-            <div className="bg-dark-purple p-4 rounded-xl">
-                <h1 className="text-xl font-bold purple-text text-start">Account Management</h1>
+            <div className="border border-white/20 p-4 rounded-xl">
+                <h1 className="text-xl font-bold text-start">Account Management</h1>
                 <div className='flex flex-col gap-2 text-sm md:w-fit mt-3'>
                   <button className='rounded-xl border-[#b99ef6] border transition-transform active:scale-95 h-min py-3 px-5 cursor-pointer'>Change password</button>
                   <button className='rounded-xl bg-red-500 transition-transform active:scale-95 h-min py-3 px-5 mt-2 cursor-pointer'>Delete account</button>
