9646e5f79f8f4bcc9700d0d31b6b3a8c3770b226
lay2ss <lais222lai@gmail.com>
2026-01-21 15:20:13 -0300

    update(Nav.tsx): refreshed design implementation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,34 +6,38 @@ import profilePicture from "../assets/icon/placeholder_profile.png";
 import menuBarIcon from "../assets/icon/menu_bar.svg";
 import closeIcon from "../assets/icon/close.svg";
 import logoname from "../assets/logo/logo_name.svg";
+import b_purple_logo from "../assets/logo/b_purple_logo.svg";
 
 const Nav = () => {
     const [isOpen, setIsOpen] = useState(false);
     const toggleMenu = () => setIsOpen(!isOpen);
   return (
-    <nav className='w-full font-inter p-5 relative mx-auto'>
-        <div className="flex justify-between w-[90vw] lg:w-[80vw] max-w-[1200px] mx-auto md:bg-[#252033] md:rounded-xl md:px-4 px-2 md:py-2">
-            <a href="/" className="cursor-pointer">
-                <img src={logoname} alt="logo name" className='w-40 h-min' />
-            </a>
-            <div className="hidden md:flex">
-                <DesktopNav/>
-            </div>
-            <div className="flex gap-3 my-auto">
-                <a href="/profile" className={`${location.pathname === "/profile" ? "border-2 border-white rounded-full" : "border-none"}`}>
-                    <img src={profilePicture} alt="profile picture" className="w-min h-min cursor-pointer"/>
+    <nav className={`w-full font-inter mx-auto fixed z-10 top-0 md:bg-white/0.5 md:backdrop-blur-sm md:border-b md:border-white/10 bg-white/0.5 ${!isOpen? "backdrop-blur-sm" : "" }`}>
+        <div className="w-full mx-auto md:px-4 px-2">
+            <div className="max-w-[1200px] flex justify-between mx-auto p-5 items-center">
+                <a href="/" className="cursor-pointer flex gap-2 items-center">
+                    <img src={b_purple_logo} alt="logo name" className='w-8 h-min' />
+                    <img src={logoname} alt="logo name" className='w-14 h-min hidden md:block'/>
                 </a>
-                <button className="hidden md:flex cursor-pointer active:scale-95">
-                    <img src={logoutIcon} alt="log out icon" />
-                </button>
-                <div className="relative flex md:hidden">
-                    <button onClick={toggleMenu} className="z-10">
-                        <img src={isOpen? closeIcon : menuBarIcon } alt="menu bar icon" className={`h-min my-auto cursor-pointer ${isOpen? 'hidden' : 'block'}`}/>
+                <div className="hidden md:flex">
+                    <DesktopNav/>
+                </div>
+                <div className="flex gap-3 my-auto">
+                    {/* <a href="/profile" className={`${location.pathname === "/profile" ? "border-2 purple-border rounded-full" : "border-none"}`}>
+                        <img src={profilePicture} alt="profile picture" className="w-min h-min cursor-pointer"/>
+                    </a> */}
+                    <button className="hidden md:flex cursor-pointer active:scale-95">
+                        <img src={logoutIcon} alt="log out icon" />
                     </button>
-                    <div className={`fixed right-0 top-0 z-5 h-screen ${isOpen? 'flex' : 'hidden'}`}>
-                        <MobileNav
-                        toggle={toggleMenu}
-                        />
+                    <div className="relative flex md:hidden">
+                        <button onClick={toggleMenu} className="z-10">
+                            <img src={isOpen? closeIcon : menuBarIcon } alt="menu bar icon" className={`h-min my-auto cursor-pointer ${isOpen? 'hidden' : 'block'}`}/>
+                        </button>
+                        <div className={`fixed right-0 top-0 z-5 h-fit w-full ${isOpen? 'flex' : 'hidden'}`}>
+                            <MobileNav
+                            toggle={toggleMenu}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
