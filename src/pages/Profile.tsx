cfae1458410ed87eb14eb73f38b94b4ed5dfd67d
lay2ss <lais222lai@gmail.com>
2026-01-20 22:30:11 -0300

    update(Profile.tsx): refreshed design implementation

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,15 +5,15 @@ import Collection from "../components/Collection";
 
 const Profile = () => {
   return (
-    <section className='h-screen w-full font-inter p-5 relative mx-auto text-white'>
-        <main className='w-[90vw] lg:w-[80vw] max-w-[1200px] rounded-tl-2xl rounded-tr-2xl mx-auto bottom-0 left-1/2 transform -translate-x-1/2  h-screen absolute border-l-3 border-r-3 border-t-3 border-[#252033] p-5'>
+    <section className='h-screen w-full font-inter p-5 mx-auto text-white'>
+        <main className='w-[90vw] lg:w-[80vw] max-w-[1200px] rounded-2xl mx-auto md:mt-2 py-20'>
         <div className="flex flex-wrap md:flex-nowrap w-full justify-center gap-5 items-center">
-          <div className="relative flex shrink-0">
+          {/* <div className="relative flex shrink-0">
             <img src="src/assets/icon/placeholder_profile2.png" alt="profile picture" className="w-min h-min"/>
             <button className="bg-black border-2 border-[#252033] w-6 h-6 rounded-full absolute top-10 left-9 cursor pointer p-0.5 cursor-pointer">
               <img src="src/assets/icon/edit.svg" alt="pencil icon"/>
             </button>
-          </div>
+          </div> */}
           <div className="flex md:gap-2  flex-wrap gap-2 md:justify-center md:flex-nowrap lg:gap-4">
             <StatWidget 
             stat={34}
@@ -35,10 +35,11 @@ const Profile = () => {
             />
           </div>
         </div>
+        <div className="w-full border-b border-white/10 py-5"/>
         <div className="flex md:justify-between flex-wrap mt-5 justify-center gap-5">
           <div>
-            <h1 className="text-2xl font-bold purple-text text-center md:text-start">Reading Goal</h1>
-            <div className="flex mt-5 border-[#252033] border-2 rounded-xl p-3 justify-center md:w-fit">
+            <h1 className="text-2xl font-bold text-center md:text-start">Reading Goal</h1>
+            <div className="flex mt-5 border-white/20 border rounded-xl p-3 justify-center md:w-fit">
               <GoalTracker 
                 current={34} 
                 max={50} 
@@ -46,7 +47,7 @@ const Profile = () => {
             </div>
           </div>
           <div className="md:w-2/3 w-full text-center md:text-start">
-            <h1 className="text-2xl font-bold purple-text">Recent Activity</h1>
+            <h1 className="text-2xl font-bold">Recent Activity</h1>
             <div className="mt-5 bg-dark-purple gap-4 flex flex-col p-3 rounded-xl">
               <BookReview
               cover="src/assets/icon/placeholder.png"
@@ -57,8 +58,9 @@ const Profile = () => {
               />
             </div>
           </div>
+          <div className="w-full border-b border-white/10 pt-5"/>
           <div className="w-full text-center md:text-start">
-            <h1 className="text-2xl font-bold purple-text">My Collections</h1>
+            <h1 className="text-2xl font-bold">My Collections</h1>
             <div className="mt-5 flex gap-4 flex-wrap justify-center md:justify-start">
               <Collection
                 name="Favorites"
