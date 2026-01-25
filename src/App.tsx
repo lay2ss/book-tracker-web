58e0e157911b978d4d4693c698a15e46b324aa5c
lay2ss <lais222lai@gmail.com>
2026-01-25 16:25:54 -0300

    feat: integrate CollectionDetails into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,12 @@ import Home from "./pages/Home";
 import Profile from './pages/Profile';
 import Settings from "./pages/Settings";
 import AddBook from "./components/AddBook";
+import CollectionDetails from "./components/CollectionDetails";
 
 function AppLayout() {
   const location = useLocation();
   const showNav = ["/", "/home", "/profile", "/settings"].includes(location.pathname) ||
-  location.pathname.startsWith("/book/");
+  location.pathname.startsWith("/book/") || location.pathname.startsWith("/collection/")
 
   return (
     <>
@@ -21,6 +22,7 @@ function AppLayout() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/book/add/:id" element={<AddBook />} />
+        <Route path="/collection/:id/:qnt" element={<CollectionDetails />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
