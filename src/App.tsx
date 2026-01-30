2a9ea76346fa518506d71b48456f43e53de91e06
lay2ss <lais222lai@gmail.com>
2026-01-29 22:43:11 -0300

    feat: add createCollection.tsx route in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Profile from './pages/Profile';
 import Settings from "./pages/Settings";
 import AddBook from "./components/AddBook";
 import CollectionDetails from "./components/CollectionDetails";
+import CreateCollection from "./components/CreateCollection";
 
 function AppLayout() {
   const location = useLocation();
@@ -23,6 +24,7 @@ function AppLayout() {
         <Route path="/settings" element={<Settings />} />
         <Route path="/book/add/:id" element={<AddBook />} />
         <Route path="/collection/:id/:qnt" element={<CollectionDetails />} />
+        <Route path="/collection/create" element={<CreateCollection />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
