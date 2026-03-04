330c64abb56deb8554490a6ec9236d6bd390bf9c
lay2ss <lais222lai@gmail.com>
2026-03-03 22:36:11 -0300

    feat: create ResetPassword.tsx

---
+++ src/pages/ResetPassword.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const ResetPassword = () => {
+
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePassword = () => setShowPassword(!showPassword);
+
+  return (
+    <section className="flex h-screen w-full justify-start flex-col font-inter text-white pt-20">
+      <div className='flex mx-auto items-center gap-2 pb-5'>
+        <img src="src/assets/logo/b_purple_logo.svg" alt="logo name" className='w-13'/>
+        <img src="src/assets/logo/logo_name.svg" alt="logo name" className='w-23' />
+      </div> 
+        <main className="w-[90vw] md:w-[60vw] lg:w-[50vw] rounded-2xl h-min gray-shadow mx-auto p-5 md:p-10">
+          <form className="form-style xl:max-w-1/2 mx-auto">
+              <h2 className="font-bold text-2xl mb-4">Reset password</h2>
+              <p className="mb-4">Enter your new password</p>
+              <div className="relative w-full">
+                <input type={showPassword ? 'text' : 'password'} placeholder="Password" className="input-style"/>
+                <button type="button" onClick={togglePassword} className="right-3 top-5 icon-style">
+                  <img src={showPassword ? "src/assets/icon/visibility_off.svg" : "src/assets/icon/visibility.svg"} alt="view" className="w-5" />
+                </button>
+              </div>
+              <div className="relative w-full">
+              </div>
+              <button type='button' className="button-style mt-3 cursor-pointer text-[#252033]">
+                Continue
+              </button>
+            </form>
+        </main>
+    </section>
+  )
+}
+
+export default ResetPassword
