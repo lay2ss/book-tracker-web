a34d2c4a7c751da8bef1741b6f2e115d16a5dc94
lay2ss <lais222lai@gmail.com>
2026-03-17 23:20:17 -0300

    refactor(App.tsx): wrap application with AuthProvider and protect private routes

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,30 @@ import CollectionDetails from "./components/CollectionDetails";
 import CreateCollection from "./components/CreateCollection";
 import ForgotPassword from "./pages/ForgotPassword";
 import ChangePassword from "./pages/ChangePassword";
+import { useAuth, AuthProvider } from './contexts/AuthContext';
+import { PrivateRoute } from "./routes/PrivateRoute";
 
 function AppLayout() {
   const location = useLocation();
-  const showNav = ["/", "/home", "/profile", "/settings", "/change-password"].includes(location.pathname) ||
+  const { token } = useAuth();
+  const showNav = token && ["/", "/home", "/profile", "/settings", "/change-password"].includes(location.pathname) ||
   location.pathname.startsWith("/book/") || location.pathname.startsWith("/collection/")
 
   return (
     <>
       {showNav && <Nav />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/change-password" element={<ChangePassword />} />
-        <Route path="/book/add/:id" element={<AddBook />} />
-        <Route path="/collection/:id/:qnt" element={<CollectionDetails />} />
-        <Route path="/collection/create" element={<CreateCollection />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+        <Route path="/settings" element={<PrivateRoute><Settings /></PrivateRoute>} />
+        <Route path="/change-password" element={<PrivateRoute><ChangePassword /></PrivateRoute>} />
+        <Route path="/book/add/:id" element={<PrivateRoute><AddBook /></PrivateRoute>} />
+        <Route path="/collection/:id/:qnt" element={<PrivateRoute><CollectionDetails /></PrivateRoute>} />
+        <Route path="/collection/create" element={<PrivateRoute><CreateCollection /></PrivateRoute>} />
       </Routes>
     </>
   );
@@ -38,7 +42,9 @@ function AppLayout() {
 export default function App() {
   return (
     <Router>
-      <AppLayout />
+      <AuthProvider>
+        <AppLayout />
+      </AuthProvider>
     </Router>
   );
 }
