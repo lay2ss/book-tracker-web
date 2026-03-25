7ece69854fdcfe734b293cc773d89aa9d6eb0498
lay2ss <lais222lai@gmail.com>
2026-03-25 00:08:34 -0300

    feat: create api.ts to handle api requests

---
+++ src/services/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL; 
+
+export const api = axios.create({
+  baseURL: VITE_API_BASE_URL
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token'); 
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
