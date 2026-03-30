eb85bcd212c5160b1f01ddd097716bfa5064a3f1
lay2ss <lais222lai@gmail.com>
2026-03-30 16:39:23 -0300

    feat: update DatePicker.tsx and Rating.tsx to handle onChange events

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+interface DatePickerProps {
+  onDateChange: (month: number, year: number) => void;
+}
+
 const months = [
   "January", "February", "March", "April",
   "May", "June", "July", "August",
@@ -12,13 +16,20 @@ const currentYear = today.getFullYear();
 
 const years = Array.from({ length: currentYear - 1999 }, (_, i) => 2000 + i);
 
-const DatePicker: React.FC = () => {
+const DatePicker: React.FC<DatePickerProps> = ({ onDateChange }) =>{
+
   const [month, setMonth] = useState<number>();
   const [year, setYear] = useState<number>();
 
+  const updateDate = (newMonth: number, newYear: number) => {
+    setMonth(newMonth);
+    setYear(newYear);
+    onDateChange(newMonth, newYear);
+  };
+
   return (
     <div className="w-fit flex gap-2">
-      <select value={month} onChange={(e) => setMonth(Number(e.target.value))} className="picker-style w-30.25">
+      <select value={month} onChange={(e) => updateDate(Number(e.target.value), Number(year))} className="picker-style w-30.25">
         {year === currentYear? months.slice(0, currentMonth).map((m, i) => (
           <option key={i} value={i}>{m}</option>
         ))  : months.map((m, i) => (
@@ -26,7 +37,7 @@ const DatePicker: React.FC = () => {
         ))}
       </select>
 
-      <select value={year} onChange={(e) => setYear(Number(e.target.value))} className="picker-style">
+      <select value={year} onChange={(e) => updateDate(Number(month), Number(e.target.value))} className="picker-style">
         {years.map((y) => (
           <option key={y} value={y}>{y}</option>
         ))}
