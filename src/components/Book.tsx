57674a96ddd68479ff1b5108cdfd3d0cf3da514e
lay2ss <lais222lai@gmail.com>
2026-04-06 23:13:48 -0300

    update Book.tsx

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -11,10 +11,10 @@ interface BookProps{
 const Book: React.FC<BookProps> = ({ current, total, cover, title, show }) =>{
   return (
         <div className="flex shrink-0 text-center cursor-pointer">
-            <div className="flex gap-2">
-                <div>
-                    <img src={cover} alt="book cover" className="w-23 h-35 sm:w-fit sm:h-fit"/>
-                    <h2 className="max-w-22.5 sm:max-w-32.5 pt-2">{title}</h2>
+            <div className="flex">
+                <div className="shrink-0">
+                    <img src={cover} alt="book cover" className="max-w-35 w-fit h-fit"/>
+                    <h2 className="max-w-22.5 sm:max-w-32.5 pt-2 mx-auto">{title}</h2>
                 </div>
                 <div className={`flex flex-col justify-center ${show}`}>
                     <CircularProgress currentPage={current} totalPages={total} />
