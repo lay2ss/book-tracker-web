02884047465ff16f6d64898fb702f733c308eba8
lay2ss <lais222lai@gmail.com>
2026-04-11 17:06:29 -0300

    update Collection.tsx

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -1,5 +1,4 @@
 import type React from "react";
-import { Link } from "react-router-dom";
 
 interface CollectionProps{
     name: string;
@@ -8,12 +7,10 @@ interface CollectionProps{
 
 const Collection: React.FC<CollectionProps> = ({name, qnt}) => {
   return (
-    <Link to={`/collection/${name.toLowerCase()}/${qnt.toString()}`} className="w-full sm:w-50">
       <div className="bg-dark-purple rounded-xl w-full p-3 sm:w-50 cursor-pointer">
           <h2>{name}</h2>
           <p className="opacity-65 text-sm">{qnt} books</p>
       </div>
-    </Link>
   )
 }
 
