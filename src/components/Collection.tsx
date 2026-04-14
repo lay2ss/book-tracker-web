04bbfc4b0764daf787c6b920c837c946a63ebe5b
lay2ss <lais222lai@gmail.com>
2026-04-14 16:39:25 -0300

    update Collection.tsx

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -9,7 +9,7 @@ const Collection: React.FC<CollectionProps> = ({name, qnt}) => {
   return (
       <div className="bg-dark-purple rounded-xl w-full p-3 sm:w-50 cursor-pointer">
           <h2>{name}</h2>
-          <p className="opacity-65 text-sm">{qnt} books</p>
+          <p className="opacity-65 text-sm">{qnt >=1 ? qnt + " books" : "0"}</p>
       </div>
   )
 }
