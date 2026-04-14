4241e28140132e9c2256858bf9cb238883626faf
lay2ss <lais222lai@gmail.com>
2026-04-14 16:47:50 -0300

    feat(CreateCollection.tsx): create function to handle collection creation

--- src/components/CreateCollection.tsx
+++ src/components/CreateCollection.tsx
@@ -1,6 +1,30 @@
-import addIcon from "../assets/icon/add.svg"
+import addIcon from "../assets/icon/add.svg";
+import { useState } from "react";
+import { createCollection } from "../services/bookService";
+import Loading from "./Loading";
+import { useNavigate } from "react-router-dom";
 
 const CreateCollection = () => {
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+
+  const handleCreate = async () => {
+
+          setLoading(true);
+  
+          try{
+              await createCollection(
+              name
+          );
+          alert("Collection created");
+          navigate('/profile');
+          } catch (err) {
+              console.error(err);
+          }  finally {
+              setLoading(false);
+          }     
+      };
 
   return (
     <section className="section-wrapper">
@@ -11,7 +35,15 @@ const CreateCollection = () => {
             </div>
             <div className="w-full flex justify-center">
                 <div className="mt-5 md:w-1/2 w-full">
-                    <input type="text" name="" id="name" placeholder="Enter collection name" maxLength={50} className="input-style"/>
+                    <input
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} 
+                    type="text" 
+                    name="" 
+                    id="name" 
+                    placeholder="Enter collection name" 
+                    maxLength={50} 
+                    className="input-style"/>
                     <label htmlFor="name"></label>
                 </div>
             </div>
@@ -26,8 +58,11 @@ const CreateCollection = () => {
           </div>
           <div className="w-full border-b border-white/10 pt-5"/>
           <div className="mt-10 flex gap-2 md:justify-end">
-            <button className="addButtonActived w-2/3 transition-transform active:scale-98 md:w-60">Create</button>
-            <button className="addButton w-1/3 transition-transform active:scale-98 md:w-35">Cancel</button>
+            <button onClick={handleCreate} disabled={loading} className="addButtonActived w-2/3 transition-transform active:scale-98 md:w-60">{loading? 
+                                    (<Loading/>) 
+                                    : 
+                                    (<p>Create</p>)}</button>
+            <button onClick={() => navigate('/profile')} className="addButton w-1/3 transition-transform active:scale-98 md:w-35">Cancel</button>
           </div>
         </main>
     </section>
