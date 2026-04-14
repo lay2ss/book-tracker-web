9700c1b8c186023722bea5772f85bb1c03dbf184
lay2ss <lais222lai@gmail.com>
2026-04-14 16:39:11 -0300

    update Book.tsx

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,4 +1,5 @@
 import CircularProgress from "./CircularProgress";
+import closeIcon from "../assets/icon/close.svg";
 
 interface BookProps{
     current?: number;
@@ -6,15 +7,21 @@ interface BookProps{
     cover?: string;
     title: string;
     show?: string;
+    showX?: string;
 }
 
-const Book: React.FC<BookProps> = ({ current, total, cover, title, show }) =>{
+const Book: React.FC<BookProps> = ({ current, total, cover, title, show, showX }) =>{
   return (
-        <div className="flex shrink-0 text-center cursor-pointer">
+        <div className="relative">
+            <div className={`flex shrink-0 text-center cursor-pointer ${showX}`}>
+                <div className="bg-dark border purple-border rounded-md absolute top-2 right-2 cursor-pointer">
+                    <img src={closeIcon} alt="close icon"/>
+                </div>
+            </div>
             <div className="flex gap-3">
                 <div className="shrink-0">
                     <img src={cover} alt="book cover" className="max-w-35 w-fit h-fit"/>
-                    <h2 className="max-w-22.5 sm:max-w-32.5 pt-2 mx-auto">{title}</h2>
+                    <h2 className="max-w-22.5 sm:max-w-32.5 pt-2 mx-auto text-center">{title}</h2>
                 </div>
                 <div className={`flex flex-col justify-center ${show}`}>
                     <CircularProgress currentPage={current} totalPages={total} />
