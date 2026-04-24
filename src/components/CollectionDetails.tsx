b6898a481530623034e79428a834854929b00e73
lay2ss <lais222lai@gmail.com>
2026-04-24 15:02:27 -0300

    feat(CollectionDetails.tsx): implement collection management and enhance details view

--- src/components/CollectionDetails.tsx
+++ src/components/CollectionDetails.tsx
@@ -1,17 +1,70 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import addIcon from "../assets/icon/add.svg";
-import editIcon from "../assets/icon/edit.svg";
 import Loading from "./Loading";
-import { getCollectionById } from "../services/bookService";
+import { getCollectionById, removeBookFromCollection, deleteCollection, updateCollectionName} from "../services/bookService";
 import { useState, useEffect } from "react";
 import Book from "./Book";
+import Dropdown from "./Dropdown";
+import AddCard from "./AddCard";
 
 const CollectionDetails = () => {
   const { id } = useParams<{ id: string }>();
   const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [showEdit, setshowEdit] = useState(false);
+  const [showAddCard, setShowAddCard] = useState(false);
   const [collection, setCollection] = useState<any>({});
   const [books, setBooks] = useState<any[]>([]); 
+  const navigate = useNavigate();
 
+  const handleRemove = async (bookId: any) => {
+    try {
+      setLoading(true);
+      await removeBookFromCollection(bookId, id);
+      alert("Book removed");
+      window.location.reload();
+    } catch (error) {
+      console.error("Failed to remove book:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDelete = async () => {
+
+      setLoading(true);
+
+      try{
+          await deleteCollection(
+          id
+      );
+      alert("Collection deleted");
+      navigate('/profile');
+      } catch (err) {
+          console.error(err);
+      }  finally {
+          setLoading(false);
+      }     
+  };
+
+  const handleEdit = async () => {
+
+    setLoading(true);
+
+    try{
+        await updateCollectionName(
+        name,
+        id
+    );
+    alert("Collection name updated");
+    window.location.reload();
+    } catch (err) {
+        console.error(err);
+    }  finally {
+        setLoading(false);
+    }     
+  };
+  
   useEffect(() => {
       const loadCollectionDetails = async () => {
         try {
@@ -40,22 +93,49 @@ const CollectionDetails = () => {
       <main className="main-wrapper md:py-25">
         {loading? <Loading/> :
           <div>
+
+            <div className={`z-10 ${showEdit? 'relative' : 'hidden'}`}>
+              <div className="absolute border border-white/20 p-6 rounded-xl bg-[#1a191b] left-1/2 -translate-x-1/2">
+                <input 
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                type="text" 
+                placeholder="Enter new collection name"
+                className="p-3 rounded-md"/>
+              <div className="flex gap-2 pt-5 justify-end">
+                <button onClick={handleEdit} className='h-min py-3 px-5 cursor-pointer text-[#1A1625] addButtonActived'>Save</button>
+                <button onClick={() => setshowEdit(!showEdit)} className='h-min py-3 px-5 cursor-pointer addButton'>Cancel</button>
+              </div>
+              </div>
+            </div>
+
             <div className="flex flex-col text-center">
               <div className="flex mx-auto gap-3 items-center">
                 <h1 className="text-xl font-bold">{collection? capitalizeFirstLetter(collection.name) : ""}</h1>
-                <img className="h-5 cursor-pointer" src={editIcon} alt="edit icon" />
+                <Dropdown 
+                onDelete={() => handleDelete()}
+                onEdit={() => setshowEdit(!showEdit)} />
               </div>
-              <p className="opacity-65">{Number(qnt) >= 1? qnt + " books" : "no books added yet"}</p>
+              <p className="opacity-65">{Number(qnt)? qnt == 1? qnt + " book" : qnt + " books" : "no books added"}</p>
             </div>
-            <div className="flex gap-5 flex-wrap mt-5">
-              {books.map((book) => (<Book key={book.id} title={book.title} cover={book.coverImage} show="hidden"/>))}
+            <div className="flex gap-4 flex-wrap mt-5">
+              {books.map((book) => (<Book
+              hoverTitle={book.title}
+              showHover="" 
+              remove={() => handleRemove(book.id)} 
+              key={book.id} cover={book.coverImage}
+              show="hidden"/>))}
             </div>
-            <button className="flex w-full justify-center mx-auto border-white/20 border cursor-pointer rounded-md hover:border-[#b99ef6] transition-transform active:scale-95 sm:w-fit sm:h-fit items-center mt-5 xs:px-10 p-2">
+            <button disabled={showEdit} className={`flex w-full justify-center mx-auto border-white/20 border cursor-pointer rounded-md hover:border-[#b99ef6] transition-transform active:scale-95 sm:w-fit sm:h-fit items-center mt-5 xs:px-10 p-2 ${showAddCard? "hidden" : "flex"}`} onClick={() => setShowAddCard(!showAddCard)}>
               <div className="flex items-center gap-2">  
                   <img src={addIcon} alt="add icon" className="h-min"/>
                   <p>Add Book</p>
               </div>
             </button>
+            <div className={`${showAddCard? "absolute top-20 left-1/2 -translate-x-1/2" : "hidden"}`}>
+              <AddCard 
+              onCancel={() => setShowAddCard(!showAddCard)}/>
+            </div>
           </div>
         }
       </main>
