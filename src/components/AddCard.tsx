1233a1730e1c05be436aef737a8df9219ff55d96
lay2ss <lais222lai@gmail.com>
2026-04-27 23:09:53 -0300

    feat(AddCard.tsx): handle multiple selections for collection updates

--- src/components/AddCard.tsx
+++ src/components/AddCard.tsx
@@ -1,4 +1,4 @@
-import { getBooks } from "../services/bookService";
+import { getBooks, addBooksToCollection } from "../services/bookService";
 import { useEffect, useState } from "react";
 import Loading from "./Loading";
 import Book from "./Book";
@@ -6,12 +6,14 @@ import placeHolder from "../assets/icon/placeholder.png";
 
 interface AddCardProps{
     onCancel: any;
+    collectionId: any;
 }
 
-const AddCard: React.FC<AddCardProps> = ({onCancel}) => {
+const AddCard: React.FC<AddCardProps> = ({onCancel, collectionId}) => {
 
     const [loading, setLoading] = useState(false);
     const [books, setBooks] = useState<any[]>([]);
+    const [selectedBookIds, setSelectedBookIds] = useState<string[]>([]);
 
     useEffect(() => {
     const loadBooks = async () => {
@@ -29,24 +31,48 @@ const AddCard: React.FC<AddCardProps> = ({onCancel}) => {
     loadBooks();
     }, []);
 
+    const handleSelectBook = (id: string) => {
+    setSelectedBookIds((prev) => 
+        prev.includes(id) 
+        ? prev.filter(bookId => bookId !== id) 
+        : [...prev, id] 
+    );
+  };
+
+  const handleSaveToCollection = async () => {
+    try {
+        setLoading(true);
+        await addBooksToCollection(selectedBookIds, collectionId);
+            alert("Books added!");
+            window.location.reload();
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
+  };
+
   return (
     <main className="relative min-w-80">
         <div className="border border-white/20 p-4 rounded-xl bg-[#1a191b]">
         {loading? <Loading /> :
         (<div className="flex flex-wrap gap-3 sm:gap-5 font-inter text-sm sm:text-[16px] overflow-y-auto max-w-200 max-h-130">
                 {books.map((book) => (
-                    <Book key={book.id}  
+                    <Book key={book.id}
+                    id={book.id}  
+                    select={true}
                     cover={book.coverImage || placeHolder}
                     show="hidden"
                     showX="hidden"
                     showHover=""
                     hoverTitle={book.title}
-                    select={true}/>  
+                    isSelected={selectedBookIds.includes(book.id)}
+                    onSelect={handleSelectBook}/>  
                 ))}
             </div>
         )}
             <div className="flex gap-2 pt-5 justify-end">
-                <button className='h-min py-3 px-5 cursor-pointer text-[#1A1625] addButtonActived'>Add</button>
+                <button onClick={handleSaveToCollection} disabled={selectedBookIds.length === 0} className='h-min py-3 px-5 cursor-pointer text-[#1A1625] addButtonActived'>Add</button>
                 <button onClick={onCancel} className='h-min py-3 px-5 cursor-pointer addButton'>Cancel</button>
             </div>
         </div>
