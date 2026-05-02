b8ae596d2f6c820c20a231508a7fb7f06344076c
lay2ss <lais222lai@gmail.com>
2026-05-02 17:44:04 -0300

    feat(Collection.tsx): lift selection state to parent

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -3,11 +3,14 @@ import type React from "react";
 interface CollectionProps{
     name: string;
     qnt: number;
+    isSelected?: boolean;
+    onSelect?: (id: string) => void;
+    id?: string;
 }
 
-const Collection: React.FC<CollectionProps> = ({name, qnt}) => {
+const Collection: React.FC<CollectionProps> = ({name, qnt, isSelected, onSelect, id}) => {
   return (
-      <div className="bg-dark-purple rounded-xl w-full p-3 sm:w-50 cursor-pointer">
+      <div onClick={() => onSelect?.(String(id))} className={`bg-dark-purple rounded-xl w-full p-3 sm:w-50 cursor-pointer ${isSelected? 'border-2 rounded-md purple-border' : ''}`}>
           <h2>{name}</h2>
           <p className="opacity-65 text-sm">{Number(qnt)? qnt == 1? qnt + " book" : qnt + " books" : "no books added"}</p>
       </div>
