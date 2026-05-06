897b24624c4a8d9717f33b462207c14e3fc19cca
lay2ss <lais222lai@gmail.com>
2026-05-05 23:56:07 -0300

    feat(AddCardCollections.tsx): reset selection on cancel and disable background actions

--- src/components/AddCardCollections.tsx
+++ src/components/AddCardCollections.tsx
@@ -32,28 +32,30 @@ const AddCardCollections: React.FC<AddCardProps> = ({onCancel, isSelected, onSel
   }, []);
 
   return (
-    <main className="relative min-w-80">
-        <div className="border border-white/20 p-4 rounded-xl bg-[#1a191b]">
-        {loading? <Loading /> :
-        (<div className="flex flex-wrap gap-3 sm:gap-5 font-inter text-sm sm:text-[16px] overflow-y-auto max-w-200 max-h-130">
-            {collections.map((collection) => (
-                        <Collection
-                        key={collection.id}
-                        name={collection.name}
-                        id={collection.id}
-                        qnt={collection._count.books}
-                        isSelected={isSelected.includes(collection.id)}
-                        onSelect={onSelect}
-                        />
-                        ))}
-            </div>
-        )}
-            <div className="flex gap-2 pt-5 justify-end">
-                <button onClick={onAdd} disabled={false} className='h-min py-3 px-5 cursor-pointer text-[#1A1625] addButtonActived'>Add</button>
-                <button onClick={onCancel} className='h-min py-3 px-5 cursor-pointer addButton'>Cancel</button>
-            </div>
-        </div>
-    </main>
+    <section className="font-inter p-5 text-white absolute z-8 top-30 h-full bg-white/0.1 backdrop-blur-xs w-full left-1/2 transform -translate-x-1/2">
+      <main className="relative w-fit min-w-80 mx-auto">
+          <div className="border border-white/20 p-4 rounded-xl bg-[#1a191b]">
+          {loading? <Loading /> :
+          (<div className="flex flex-wrap gap-3 sm:gap-5 font-inter text-sm sm:text-[16px] overflow-y-auto max-w-200 max-h-130">
+              {collections.map((collection) => (
+                          <Collection
+                          key={collection.id}
+                          name={collection.name}
+                          id={collection.id}
+                          qnt={collection._count.books}
+                          isSelected={isSelected.includes(collection.id)}
+                          onSelect={onSelect}
+                          />
+                          ))}
+              </div>
+          )}
+              <div className="flex gap-2 pt-5 justify-end">
+                  <button onClick={onAdd} disabled={false} className='h-min py-3 px-5 cursor-pointer text-[#1A1625] addButtonActived'>Add</button>
+                  <button onClick={onCancel} className='h-min py-3 px-5 cursor-pointer addButton'>Cancel</button>
+              </div>
+          </div>
+      </main>
+    </section>
   )
 }
 
