04b1448038973eeefd1b6f89b7eefe94c43b9bcf
lay2ss <lais222lai@gmail.com>
2026-05-05 23:55:43 -0300

    feat(AddCardBooks.tsx): reset selection on cancel and disable background actions

--- src/components/AddCardBooks.tsx
+++ src/components/AddCardBooks.tsx
@@ -52,30 +52,39 @@ const AddCardBooks: React.FC<AddCardProps> = ({onCancel, collectionId}) => {
         }
   };
 
+  const handleScrollTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        })
+    };
+
   return (
-    <main className="relative min-w-80">
-        <div className="border border-white/20 p-4 rounded-xl bg-[#1a191b]">
-        {loading? <Loading /> :
-        (<div className="flex flex-wrap gap-3 sm:gap-5 font-inter text-sm sm:text-[16px] overflow-y-auto max-w-200 max-h-130">
-                {books.map((book) => (
-                    <Book key={book.id}
-                    id={book.id}  
-                    cover={book.coverImage || placeHolder}
-                    show="hidden"
-                    showX="hidden"
-                    showHover=""
-                    hoverTitle={book.title}
-                    isSelected={selectedBookIds.includes(book.id)}
-                    onSelect={handleSelectBook}/>  
-                ))}
+    <section className="font-inter p-5 text-white absolute z-8 top-30 h-full bg-white/0.1 backdrop-blur-xs w-full left-1/2 transform -translate-x-1/2">
+        <main className="relative w-fit min-w-80 mx-auto">
+            <div className="border border-white/20 p-4 rounded-xl bg-[#1a191b]">
+            {loading? <Loading /> :
+            (<div className="flex flex-wrap gap-3 sm:gap-5 font-inter text-sm sm:text-[16px] overflow-y-auto max-w-200 max-h-130">
+                    {books.map((book) => (
+                        <Book key={book.id}
+                        id={book.id}  
+                        cover={book.coverImage || placeHolder}
+                        show="hidden"
+                        showX="hidden"
+                        showHover=""
+                        hoverTitle={book.title}
+                        isSelected={selectedBookIds.includes(book.id)}
+                        onSelect={handleSelectBook}/>  
+                    ))}
+                </div>
+            )}
+                <div className="flex gap-2 pt-5 justify-end">
+                    <button onClick={() => {handleSaveToCollection(), handleScrollTop()}} disabled={selectedBookIds.length === 0} className='h-min py-3 px-5 cursor-pointer text-[#1A1625] addButtonActived'>Add</button>
+                    <button onClick={() => {setSelectedBookIds([]), onCancel()}} className='h-min py-3 px-5 cursor-pointer addButton'>Cancel</button>
+                </div>
             </div>
-        )}
-            <div className="flex gap-2 pt-5 justify-end">
-                <button onClick={handleSaveToCollection} disabled={selectedBookIds.length === 0} className='h-min py-3 px-5 cursor-pointer text-[#1A1625] addButtonActived'>Add</button>
-                <button onClick={onCancel} className='h-min py-3 px-5 cursor-pointer addButton'>Cancel</button>
-            </div>
-        </div>
-    </main>
+        </main>
+    </section>
   )
 }
 
