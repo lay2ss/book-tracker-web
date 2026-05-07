2754b26899a1648e217672f9898a43d19e230db5
lay2ss <lais222lai@gmail.com>
2026-05-07 16:43:03 -0300

    feat(CollectionDetails.tsx): update Book component with new prop

--- src/components/CollectionDetails.tsx
+++ src/components/CollectionDetails.tsx
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import addIcon from "../assets/icon/add.svg";
 import Loading from "./Loading";
 import { getCollectionById, removeBookFromCollection, deleteCollection, updateCollectionName} from "../services/bookService";
@@ -6,6 +6,8 @@ import { useState, useEffect } from "react";
 import Book from "./Book";
 import Dropdown from "./Dropdown";
 import AddCardBooks from "./AddCardBooks";
+import placeHolder from "../assets/icon/placeholder.png";
+import arrowBackIcon from "../assets/icon/arrow_back.svg";
 
 const CollectionDetails = () => {
   const { id } = useParams<{ id: string }>();
@@ -64,6 +66,13 @@ const CollectionDetails = () => {
         setLoading(false);
     }     
   };
+
+  const handleScrollTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        })
+    };
   
   useEffect(() => {
       const loadCollectionDetails = async () => {
@@ -94,7 +103,7 @@ const CollectionDetails = () => {
         {loading? <Loading/> :
           <div>
 
-            <div className={`z-10 ${showEdit? 'relative' : 'hidden'}`}>
+            <div className={`z-10 ${showEdit? 'font-inter p-5 text-white absolute z-8 top-30 h-full bg-white/0.1 backdrop-blur-xs w-full left-1/2 transform -translate-x-1/2' : 'hidden'}`}>
               <div className="absolute border border-white/20 p-6 rounded-xl bg-[#1a191b] left-1/2 -translate-x-1/2">
                 <input 
                 value={name}
@@ -104,13 +113,14 @@ const CollectionDetails = () => {
                 className="p-3 rounded-md"/>
               <div className="flex gap-2 pt-5 justify-end">
                 <button onClick={handleEdit} className='h-min py-3 px-5 cursor-pointer text-[#1A1625] addButtonActived'>Save</button>
-                <button onClick={() => setshowEdit(!showEdit)} className='h-min py-3 px-5 cursor-pointer addButton'>Cancel</button>
+                <button onClick={() => {setshowEdit(!showEdit), setName("")}} className='h-min py-3 px-5 cursor-pointer addButton'>Cancel</button>
               </div>
               </div>
             </div>
 
             <div className="flex flex-col text-center">
-              <div className="flex mx-auto gap-3 items-center">
+              <div className="w-full flex mx-auto items-center justify-between">
+                <img src={arrowBackIcon} alt="arrow back icon" className="cursor-pointer" onClick={() => navigate(-1)}/>
                 <h1 className="text-xl font-bold">{collection? capitalizeFirstLetter(collection.name) : ""}</h1>
                 <Dropdown 
                 onDelete={() => handleDelete()}
@@ -119,20 +129,25 @@ const CollectionDetails = () => {
               <p className="opacity-65">{Number(qnt)? qnt == 1? qnt + " book" : qnt + " books" : "no books added"}</p>
             </div>
             <div className="flex gap-4 flex-wrap mt-5">
-              {books.map((book) => (<Book
-              hoverTitle={book.title}
-              showHover="" 
-              remove={() => handleRemove(book.id)} 
-              key={book.id} cover={book.coverImage}
-              show="hidden"/>))}
+              {books.map((book) => (
+                  <Book
+                  key={book.externalId}
+                  hoverTitle={book.title}
+                  showHover="" 
+                  remove={() => handleRemove(book.id)} 
+                  cover={book.coverImage || placeHolder}
+                  show="hidden"
+                  goToBook={() => navigate(`/book/edit/${book.externalId}/${book.id}`)}
+                  />
+              ))}
             </div>
             <button disabled={showEdit} className={`flex w-full justify-center mx-auto border-white/20 border cursor-pointer rounded-md hover:border-[#b99ef6] transition-transform active:scale-95 sm:w-fit sm:h-fit items-center mt-5 xs:px-10 p-2 ${showAddCard? "hidden" : "flex"}`} onClick={() => setShowAddCard(!showAddCard)}>
-              <div className="flex items-center gap-2">  
+              <div onClick={handleScrollTop} className="flex items-center gap-2">  
                   <img src={addIcon} alt="add icon" className="h-min"/>
                   <p>Add Book</p>
               </div>
             </button>
-            <div className={`${showAddCard? "absolute top-20 left-1/2 -translate-x-1/2" : "hidden"}`}>
+            <div className={`${showAddCard? "flex" : "hidden"}`}>
               <AddCardBooks 
               onCancel={() => setShowAddCard(!showAddCard)}
               collectionId={id}/>
