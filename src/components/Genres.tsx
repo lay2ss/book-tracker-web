3a41f91cb02aeec0fb0e721aa62f338887b93f0c
lay2ss <lais222lai@gmail.com>
2026-05-19 23:38:22 -0300

    feat(Genres.tsx): convert Genres to a controlled component for state lifting

--- src/components/Genres.tsx
+++ src/components/Genres.tsx
@@ -1,16 +1,15 @@
-import { useState } from "react";
-
 interface GenresProps{
     genre: string;
+    isSelected: boolean; 
+    onClick: () => void;
 }
 
-const Genres: React.FC<GenresProps> = ({genre}) => {
-
-    const [isChecked, setIsChecked] = useState(false);
-    const toggleState = () => setIsChecked(!isChecked);
-
+const Genres: React.FC<GenresProps> = ({genre, isSelected, onClick}) => {
+  
   return (
-    <button onClick={toggleState} className={`px-2 py-1 purple-border border rounded-full cursor-pointer text-sm transition-transform active:scale-97 ${isChecked? "text-[#252033] bg-[#b99ef6] border-none" : ""}`}>{genre}</button>
+    <button type="button" 
+    onClick={onClick} 
+    className={`px-2 py-1 purple-border border rounded-full cursor-pointer text-sm transition-transform active:scale-97 ${isSelected? "text-[#252033] bg-[#b99ef6] border-none" : ""}`}>{genre}</button>
   )
 }
 
