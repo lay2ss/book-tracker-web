95666099fb919ff998df21c77c1faf8efde01653
lay2ss <lais222lai@gmail.com>
2026-05-27 15:10:48 -0300

    feat(ResetPassword.tsx): connect reset password page to the backend

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -3,11 +3,43 @@ import visibility_off from "../assets/icon/visibility_off.svg";
 import visibility from "../assets/icon/visibility.svg";
 import logoname from "../assets/logo/logo_name.svg";
 import b_purple_logo from "../assets/logo/b_purple_logo.svg";
+import { resetPassword } from "../services/bookService";
+import { useNavigate, useParams } from "react-router-dom";
+import Loading from "../components/Loading";
 
 const ResetPassword = () => {
 
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const { token } = useParams<{ token: string }>();
+
     const [showPassword, setShowPassword] = useState(false);
     const togglePassword = () => setShowPassword(!showPassword);
+
+    const handleReset = async () => {
+
+      setLoading(true);
+  
+      try{
+        await resetPassword(
+          password,
+          token
+        )
+        alert("Password updated successfully!");
+        navigate('/login');
+      } catch (err: any) {
+          console.error(err);
+          if (err.response && err.response.data) {
+            const backendMessage = err.response.data.message || "Invalid request.";
+            alert(backendMessage);
+          } else {
+            alert("Something went wrong. Please try again later.");
+          }
+        }  finally {
+          setLoading(false);
+        }  
+    };
 
   return (
     <section className="justify-start pt-20 password-page font-inter">
@@ -20,15 +52,24 @@ const ResetPassword = () => {
               <h2 className="font-bold text-2xl mb-4">Reset password</h2>
               <p className="mb-4">Enter your new password</p>
               <div className="relative w-full">
-                <input type={showPassword ? 'text' : 'password'} placeholder="Password" className="input-style"/>
-                <button type="button" onClick={togglePassword} className="right-3 top-5 icon-style">
+                <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}  
+                type={showPassword ? 'text' : 'password'} 
+                placeholder="Password" className="input-style"/>
+                <button type="button" onClick={togglePassword} 
+                className="right-3 top-5 icon-style">
                   <img src={showPassword ? visibility_off : visibility} alt="view" className="w-5" />
                 </button>
               </div>
               <div className="relative w-full">
               </div>
-              <button type='button' className="button-style mt-3 cursor-pointer text-[#252033]">
-                Continue
+              <button 
+              disabled={loading} 
+              onClick={handleReset}
+              type='button' 
+              className="button-style mt-3 cursor-pointer text-[#252033]">
+                {loading? <Loading/> : "Reset"}
               </button>
             </form>
         </main>
