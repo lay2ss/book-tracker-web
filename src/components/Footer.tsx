b53b60f5728df07cca4913e40f7eb6be2bbcabdc
lay2ss <lais222lai@gmail.com>
2026-07-10 15:35:05 -0300

    update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import heart_icon from "../assets/icon/heart_active.svg";
 
 function Footer() {
   return (
-    <footer className="section-wrapper">
+    <footer className="w-full px-5 relative mx-auto text-white font-inter">
         <main className="w-[90vw] lg:w-[80vw] max-w-300 rounded-2xl mx-auto">
         <div className="w-full border-b border-white/10"/>
         <p className="flex gap-2 opacity-50 items-center justify-center p-5 text-xs">Made with <img className="w-4" src={heart_icon} alt="heart icon" /> by @lay2ss</p>
