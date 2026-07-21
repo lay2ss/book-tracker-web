66d4cb333c7216c1cf314ef84ce82652cbde3ef8
lay2ss <lais222lai@gmail.com>
2026-07-20 23:24:55 -0300

    feat(ForgotPassword.tsx): add temporary alert notifications

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,19 +1,24 @@
 import logoname from "../assets/logo/logo_name.svg";
 import b_purple_logo from "../assets/logo/b_purple_logo.svg";
 import { forgotPassword } from "../services/bookService";
-import { useNavigate } from "react-router-dom";
 import Loading from "../components/Loading";
 import { useState } from "react";
+import SimpleAlert from '../components/SimpleAlert';
 
 const ForgotPassword = () => {
 
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const emailSent = "If an account is associated with this email, you will receive a password reset link shortly.";
 
   const handleSend = async () => {
     if (!email.trim()) {
-    alert("Please enter your email address.");
+    setShowAlert(true);
+    setAlertMessage("Please enter your email address.");
     return;
     }
     setLoading(true);
@@ -22,15 +27,17 @@ const ForgotPassword = () => {
       await forgotPassword(
         email.trim(),
       )
-      alert("If an account is associated with this email, you will receive a password reset link shortly.");
-      navigate('/login');
+      setShowAlert(true);
+      setAlertMessage(emailSent);
     } catch (err: any) {
         console.error(err);
         if (err.response && err.response.data) {
           const backendMessage = err.response.data.message || "Invalid request.";
-          alert(backendMessage);
+          setShowAlert(true);
+          setAlertMessage(backendMessage);
         } else {
-          alert("Something went wrong. Please try again later.");
+          setShowAlert(true);
+          setAlertMessage("Something went wrong. Please try again later.");
         }
       }  finally {
         setLoading(false);
@@ -39,6 +46,14 @@ const ForgotPassword = () => {
 
   return (
     <section className="justify-start pt-20 password-page font-inter">
+        {showAlert && (
+            <SimpleAlert 
+              severity={alertMessage === emailSent? 'info' : 'warning'}
+              message={alertMessage} 
+              onClose={() => setShowAlert(false)}
+              goTo={alertMessage === emailSent? '/login' : ''}
+            />
+          )}
       <div className='flex mx-auto items-center gap-2 pb-5'>
         <img src={b_purple_logo} alt="logo name" className='w-13'/>
         <img src={logoname} alt="logo name" className='w-23' />
