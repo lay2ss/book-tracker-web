b50e39fddce133fcb847018f9ecd576ae93b8c1c
lay2ss <lais222lai@gmail.com>
2026-07-23 22:49:50 -0300

    feat(VerifyEmail.tsx): add temporary alert notifications

--- src/pages/VerifyEmail.tsx
+++ src/pages/VerifyEmail.tsx
@@ -1,16 +1,20 @@
 import logoname from "../assets/logo/logo_name.svg";
 import b_purple_logo from "../assets/logo/b_purple_logo.svg";
 import { verifyEmail } from "../services/bookService";
-import { useNavigate } from "react-router-dom";
 import Loading from "../components/Loading";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import SimpleAlert from '../components/SimpleAlert';
 
 const VerifyEmail = () => {
 
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
   const { token } = useParams<{ token: string }>();
+
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const successMessage = "Email verified successfully! You can now log in."
 
   const handleVerify = async () => {
     setLoading(true);
@@ -18,15 +22,17 @@ const VerifyEmail = () => {
       await verifyEmail(
         token,
       )
-      alert("Email verified successfully! You can now log in.");
-      navigate('/login');
+      setShowAlert(true);
+      setAlertMessage(successMessage);
     } catch (err: any) {
         console.error(err);
         if (err.response && err.response.data) {
           const backendMessage = err.response.data.message || "Invalid request.";
-          alert(backendMessage);
+          setShowAlert(true);
+          setAlertMessage(backendMessage);
         } else {
-          alert("Something went wrong. Please try again later.");
+          setShowAlert(true);
+          setAlertMessage("Something went wrong. Please try again later.");
         }
       }  finally {
         setLoading(false);
@@ -35,6 +41,14 @@ const VerifyEmail = () => {
 
   return (
     <section className="justify-start pt-20 password-page font-inter">
+      {showAlert && (
+        <SimpleAlert 
+          severity={successMessage === alertMessage? "success" : "warning"}  
+          message={alertMessage} 
+          onClose={() => setShowAlert(false)}
+          goTo={successMessage === alertMessage? "/login" : ""}
+        />
+      )}
       <div className='flex mx-auto items-center gap-2 pb-5'>
         <img src={b_purple_logo} alt="logo name" className='w-13'/>
         <img src={logoname} alt="logo name" className='w-23' />
