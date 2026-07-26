a370262e3a228aae89223de773da16c538b58769
lay2ss <lais222lai@gmail.com>
2026-07-26 17:10:48 -0300

    update Collection.tsx

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -10,7 +10,7 @@ interface CollectionProps{
 
 const Collection: React.FC<CollectionProps> = ({name, qnt, isSelected, onSelect, id}) => {
   return (
-      <div onClick={() => onSelect?.(String(id))} className={`bg-dark-purple rounded-xl w-full p-3 sm:w-50 cursor-pointer ${isSelected? 'border-2 rounded-md purple-border' : ''}`}>
+      <div onClick={() => onSelect?.(String(id))} className={`bg-dark-purple rounded-xl w-full p-3 cursor-pointer ${isSelected? 'border-2 rounded-md purple-border' : ''}`}>
           <h2>{name}</h2>
           <p className="opacity-65 text-sm">{Number(qnt)? qnt == 1? qnt + " book" : qnt + " books" : ":/"}</p>
       </div>
