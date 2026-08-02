bbc6f2e47af9b5ff4ef746107947213ac912ed3f
lay2ss <lais222lai@gmail.com>
2026-08-02 16:44:21 -0300

    feat: add new prop to StatWidget.tsx

--- src/components/StatWidget.tsx
+++ src/components/StatWidget.tsx
@@ -2,10 +2,11 @@ interface StatWidgetProps{
     stat: any;
     icon: any;
     text: string;
+    p?: string;
     alt?: string;
 }
 
-const StatWidget: React.FC<StatWidgetProps> = ({stat, icon, text, alt}) => {
+const StatWidget: React.FC<StatWidgetProps> = ({stat, icon, text, alt, p}) => {
   return (
             <div className="stats-style">
                 <div className="flex gap-2">
@@ -15,6 +16,9 @@ const StatWidget: React.FC<StatWidgetProps> = ({stat, icon, text, alt}) => {
                     </p>
                 </div>
                 <div className="purple-text mt-1">{stat}</div>
+                <p className="text-xs opacity-60 font-normal">
+                    {p}
+                </p>
             </div>
   )
 }
